f73559a09933ad3593779c1a871f08eae04a4e45
richard1230 <yongjinhu1230@gmail.com>
2020-12-05 10:51:00 +0800

    添加路由1

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,27 @@
 import styles from './App.module.scss';
+import {Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className={styles.App}>
-      个人网站
+
+
       <header className={styles.header}>
-         头部
+        <div className={styles.box}>
+          <Link to="/"><div className={styles.boxleft}>Vauxlab </div></Link>
+          <div className={styles.boxright}>
+             <Link to="/"><p>HOME</p></Link>
+            <Link to="/works"><p>WORKS</p></Link>
+          </div>
+        </div>
+
       </header>
       <section className={styles.content}>
         内容
       </section>
       <footer className={styles.footer}>
-        底部
+        <p>© <b>2020 Richard's Site</b></p>
+        <p>Welcome !!!</p>
       </footer>
     </div>
   );
