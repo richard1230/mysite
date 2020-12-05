94378d415b51f922a881af334bcb2f22e8289413
richard1230 <yongjinhu1230@gmail.com>
2020-12-05 10:23:50 +0800

    基本布局

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      个人网站
+      <header className={styles.header}>
+         头部
       </header>
+      <section className={styles.content}>
+        内容
+      </section>
+      <footer className={styles.footer}>
+        底部
+      </footer>
     </div>
   );
 }
